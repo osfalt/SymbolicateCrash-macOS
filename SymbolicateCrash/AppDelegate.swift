0507d0210d09979ee44365d12e7edc58f5bec252
Osfalt <osfalt4@gmail.com>
2018-07-14 01:30:59 +0300

    Initial commit

---
+++ SymbolicateCrash/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SymbolicateCrash
+//
+//  Created by Dre on 14/07/2018.
+//  Copyright © 2018 Andrey Sidorovnin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
---
+++ SymbolicateCrash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SymbolicateCrash
+//
+//  Created by Dre on 14/07/2018.
+//  Copyright © 2018 Andrey Sidorovnin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+
+}
